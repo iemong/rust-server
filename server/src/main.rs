0dc9db9a0e0d8ade1df4e0665374ac0d1fd5fcd4
iemong <kiyoshidainagon@gmail.com>
2021-03-15 00:08:18 +0900

    actixを使う

--- server/src/main.rs
+++ server/src/main.rs
@@ -1,5 +1,30 @@
-mod simple_server;
+use actix_web::{web, App, HttpResponse, HttpServer};
 
-fn main() {
-    simple_server::simple_server();
+fn api_config(cfg: &mut web::ServiceConfig) {
+    cfg.service(
+        web::resource("/test")
+            .route(web::get().to(|| HttpResponse::Ok().body("test")))
+            .route(web::head().to(|| HttpResponse::MethodNotAllowed())),
+    );
+}
+
+fn app_config(cfg: &mut web::ServiceConfig) {
+    cfg.service(
+        web::resource("/app")
+            .route(web::get().to(|| HttpResponse::Ok().body("app")))
+            .route(web::head().to(|| HttpResponse::MethodNotAllowed())),
+    );
+}
+
+#[actix_web::main]
+async fn main() -> std::io::Result<()> {
+    HttpServer::new(|| {
+        App::new()
+            .configure(app_config)
+            .service(web::scope("api").configure(api_config))
+            .route("/", web::get().to(|| HttpResponse::Ok().body("/")))
+    })
+    .bind("127.0.0.1:8080")?
+    .run()
+    .await
 }
